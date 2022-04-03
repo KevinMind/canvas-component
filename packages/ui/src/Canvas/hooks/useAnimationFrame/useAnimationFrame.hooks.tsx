74113b463af9704a785b8d29b3d3ad02898b20a9
kevinmind <kmeinhardt7@gmail.com>
2022-04-03 13:34:38 +0200

    fix stale easing prop

--- packages/ui/src/Canvas/hooks/useAnimationFrame/useAnimationFrame.hooks.tsx
+++ packages/ui/src/Canvas/hooks/useAnimationFrame/useAnimationFrame.hooks.tsx
@@ -1,17 +1,20 @@
-import { useMemo } from "react";
+import { useRef, useEffect } from "react";
 import {
   useRequestAnimationFrame,
   RequestAnimationFrameConfig,
 } from "../useRequestAnimationFrame";
 import { cubicBezier } from "./useAnimationFrame.utilities";
 
-type Easing = "linear" | "ease" | "ease-in" | "ease-out" | "ease-in-out";
+export type Easing = "linear" | "ease" | "ease-in" | "ease-out" | "ease-in-out";
 export interface UseAnimationFrameArgs
   extends Partial<RequestAnimationFrameConfig> {
   duration: number;
   from: number;
   to: number;
-  easing?: Easing | [number, number, number, number];
+  easing?:
+    | Easing
+    | [number, number, number, number]
+    | ((time: number) => number);
 }
 
 export function useAnimationFrame({
@@ -20,26 +23,35 @@ export function useAnimationFrame({
   easing = "linear",
   ...rest
 }: UseAnimationFrameArgs) {
-  const easingFunction = useMemo(() => {
-    if (Array.isArray(easing)) {
-      return cubicBezier(...easing);
+  const easingFunction = useRef<(curr: number) => number>((num) => num);
+
+  useEffect(() => {
+    function getEasing() {
+      if (typeof easing === "function") {
+        return easing;
+      }
+      if (Array.isArray(easing)) {
+        return cubicBezier(...easing);
+      }
+      switch (easing) {
+        case "ease":
+          return cubicBezier(0.25, 0.1, 0.25, 0.1);
+        case "ease-in":
+          return cubicBezier(0.42, 0, 1, 1);
+        case "ease-out":
+          return cubicBezier(0, 0, 0.58, 1);
+        case "ease-in-out":
+          return cubicBezier(0.42, 0, 0.58, 1);
+        case "linear":
+        default:
+          return cubicBezier(0, 0, 1, 1);
+      }
     }
-    switch (easing) {
-      case "ease":
-        return cubicBezier(0.25, 0.1, 0.25, 0.1);
-      case "ease-in":
-        return cubicBezier(0.42, 0, 1, 1);
-      case "ease-out":
-        return cubicBezier(0, 0, 0.58, 1);
-      case "ease-in-out":
-        return cubicBezier(0.42, 0, 0.58, 1);
-      case "linear":
-      default:
-        return cubicBezier(0, 0, 1, 1);
-    }
+
+    easingFunction.current = getEasing();
   }, [easing]);
 
   return useRequestAnimationFrame((curr) => {
-    return from + (to - from) * easingFunction(curr / rest.duration);
+    return from + (to - from) * easingFunction.current(curr / rest.duration);
   }, rest);
 }
--- packages/ui/src/Canvas/hooks/useAnimationFrame/useAnimationFrame.stories.tsx
+++ packages/ui/src/Canvas/hooks/useAnimationFrame/useAnimationFrame.stories.tsx
@@ -4,7 +4,10 @@ import { ComponentMeta, StoryObj } from "@storybook/react";
 import {
   useAnimationFrame,
   UseAnimationFrameArgs,
+  Easing as EasingType,
 } from "./useAnimationFrame.hooks";
+import { Circle } from "../../components/Circle";
+import { CanvasProvider } from "../../Canvas.provider";
 
 function TransitionValue(args: UseAnimationFrameArgs) {
   const [value, actions] = useAnimationFrame(args);
@@ -39,4 +42,42 @@ export const Default: TransitionValueStory = {
     infinite: true,
     auto: true,
   },
+};
+
+function RenderEasing({ easing }: { easing: EasingType }) {
+  const [x] = useAnimationFrame({
+    easing,
+    auto: true,
+    mode: "pingpong",
+    duration: 3_000,
+    from: 125,
+    to: 375,
+  });
+  return (
+    <CanvasProvider width={500} height={250} style={{ border: "1px solid" }}>
+      <Circle pos={{ x, y: 125 }} radius={125} rotation={0} />
+    </CanvasProvider>
+  );
+}
+
+type EasingStory = StoryObj<ComponentProps<typeof RenderEasing>>;
+
+export const Easing: EasingStory = {
+  args: {
+    easing: "linear",
+  },
+  argTypes: {
+    easing: {
+      description: "overwritten description",
+      table: {
+        type: {
+          summary: "something short",
+          detail: "something really really long",
+        },
+      },
+      options: ["linear", "ease", "ease-in", "ease-out", "ease-in-out"],
+      control: { type: "radio" },
+    },
+  },
+  render: (args) => <RenderEasing {...args} />,
 };
